8c25144c97d33d4ff5df000e3ec0e28e1ded71d4
jose <josecschalil@gmail.com>
2026-03-31 11:01:33 +0530

    made lot of changes to tx and rx side

--- receive.m
+++ receive.m
@@ -1,12 +1,14 @@
 clc;
 clear;
 
-fs = 48000;
-fs_sdr = 1000000;   % 1 MHz (safe)
-freq_dev = 3000;
+%% PARAMETERS
+fs = 48000;           % Audio sample rate
+fs_sdr = 1000000;     % SDR sample rate
+freq_dev = 3000;      % FM deviation
 
+%% SDR SETUP
 rx = sdrrx('Pluto');
-rx.CenterFrequency = 433.001e6;
+rx.CenterFrequency = 433e6;   % Adjust slightly if needed
 rx.BasebandSampleRate = fs_sdr;
 rx.SamplesPerFrame = 4096;
 rx.GainSource = 'Manual';
@@ -17,48 +19,77 @@ fprintf('RECEIVER STARTED\n');
 fprintf('Waiting for packets...\n');
 fprintf('==============================\n');
 
+%% BUFFER
 buffer = [];
 flag = [0 1 1 1 1 1 1 0];
 
+%% BANDPASS FILTER (AFSK: 1200–2200 Hz)
+bp = designfilt('bandpassiir', ...
+'FilterOrder', 6, ...
+'HalfPowerFrequency1', 800, ...
+'HalfPowerFrequency2', 2600, ...
+'SampleRate', fs_sdr);
+
+%% MAIN LOOP
 while true
 
-
+%% RECEIVE RF
 data = rx();
 data = double(data);
 
-% FM demod
-phase = unwrap(angle(data));
-fm = diff(phase);
-fm = fm * fs_sdr/(2*pi*freq_dev);
+%% FM DEMOD (ROBUST)
+fm = angle(data(2:end) .* conj(data(1:end-1)));
+fm = [fm; fm(end)];
 
+%% FILTER (CRITICAL)
+fm = filtfilt(bp, fm);
+
+%% ADD TO BUFFER
 buffer = [buffer; fm];
 
+%% KEEP BUFFER LIMITED
 if length(buffer) > fs_sdr
     buffer = buffer(end-fs_sdr+1:end);
 end
 
-% Downsample
-audio = resample(buffer, fs, fs_sdr);
+%% PROCESS WHEN ENOUGH DATA
+if length(buffer) < fs_sdr/2
+    continue;
+end
 
+%% DOWNSAMPLE (CLEAN)
+audio = decimate(buffer(1:fs_sdr/2), round(fs_sdr/fs));
+
+%% SLIDING WINDOW
+buffer = buffer(fs_sdr/4:end);
+
+%% DEBUG (OPTIONAL)
+% plot(audio(1:200))
+% title('Recovered Audio')
+% drawnow
+
+%% AFSK DEMOD
 bits = afsk_demodulate(audio, fs);
 
-% Detect packet
+%% FLAG DETECTION
 flag_pos = strfind(bits, flag);
 
 if length(flag_pos) < 2
     continue;
 end
 
+%% DECODE AX.25
 try
     [src, dest, msg] = ax25_decode(bits);
-    
+
     fprintf('\n📡 RECEIVED PACKET\n');
     fprintf('FROM: %s\n', src);
+    fprintf('TO  : %s\n', dest);
     fprintf('MSG : %s\n', msg);
-    
-    buffer = [];
-    
+    fprintf('==============================\n');
+
 catch
+    % Ignore invalid frames
 end
 
 
--- transmit.m
+++ transmit.m
@@ -8,24 +8,24 @@ msg = 'HELLO FROM SDR';
 frame = ax25_encode('N0CALL','APRS', msg);
 audio = afsk_modulate(frame, fs);
 
-% SDR preparation
-fs_sdr = 1000000;   % 1 MHz (safe)
-interp_factor = fs_sdr/fs;
+%% SDR PREP
+fs_sdr = 1000000;
+
 audio_up = resample(audio, fs_sdr, fs);
 
 freq_dev = 3000;
 phase = 2*pi*freq_dev * cumsum(audio_up)/fs_sdr;
 tx_signal = exp(1j*phase);
 
+%% SDR TX
 tx = sdrtx('Pluto');
-rx.CenterFrequency = 433.001e6;
+tx.CenterFrequency = 433e6;
 tx.BasebandSampleRate = fs_sdr;
 tx.Gain = 0;
 
 fprintf('==============================\n');
 fprintf('TRANSMITTER STARTED\n');
-fprintf('Sending message: %s\n', msg);
-fprintf('Press Ctrl+C to stop\n');
+fprintf('Message: %s\n', msg);
 fprintf('==============================\n');
 
 transmitRepeat(tx, tx_signal);
